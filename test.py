f93dd21498c0cccf04d7b6caa9188ea53527c9ac
Francisco Ramos <jscriptcoder@gmail.com>
2020-09-16 08:19:34 +0200

    WIP

--- test.py
+++ test.py
@@ -11,17 +11,18 @@ config.env = env
 config.env_solved = 200
 config.state_size = env.observation_space.shape[0]
 config.action_size = env.action_space.n
-config.batch_size = 512
-config.update_every = 1
+config.batch_size = 128
+config.update_every = 4
+config.max_steps = 300
 
 config.tau = 1e-2
-config.lr_actor = 1e-3
-config.lr_critic = 1e-3
-config.hidden_actor = (64, 64)
-config.hidden_critic = (512,)
+config.lr_actor = 3e-4
+config.lr_critic = 3e-4
+config.hidden_actor = (256, 256)
+config.hidden_critic = (256, 256)
 
-config.grad_clip_actor = 5
-config.grad_clip_critic = 5
+# config.grad_clip_actor = 5
+# config.grad_clip_critic = 5
 
 config.lr_alpha = 3e-4
 
